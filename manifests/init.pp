d38eac945da767b39f798dba3f064271dddcba6d
skrussell <russell@samknows.com>
2018-02-07 15:30:29 +0000

    Make the ldap_servers variable able to accept and process an array of strings, as well as just a single string

--- manifests/init.pp
+++ manifests/init.pp
@@ -251,7 +251,7 @@ class rabbitmq(
   Array $ssl_ciphers                                               = $rabbitmq::params::ssl_ciphers,
   Boolean $stomp_ensure                                            = $rabbitmq::params::stomp_ensure,
   Boolean $ldap_auth                                               = $rabbitmq::params::ldap_auth,
-  String $ldap_server                                              = $rabbitmq::params::ldap_server,
+  Variant[String,Array[String]] $ldap_server                       = $rabbitmq::params::ldap_server,
   Optional[String] $ldap_user_dn_pattern                           = $rabbitmq::params::ldap_user_dn_pattern,
   String $ldap_other_bind                                          = $rabbitmq::params::ldap_other_bind,
   Boolean $ldap_use_ssl                                            = $rabbitmq::params::ldap_use_ssl,
